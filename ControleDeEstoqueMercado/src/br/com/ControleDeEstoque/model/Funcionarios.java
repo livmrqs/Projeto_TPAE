96b625359d3d88986d71175a8347d702e9965562
Lívia Marques <livia.rodrigues.pb@compasso.com.br>
2024-07-30 18:44:16 -0300

    feat: adicionando getters e setters de Funcionarios

--- ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/model/Funcionarios.java
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/model/Funcionarios.java
@@ -15,5 +15,30 @@ public class Funcionarios extends Clientes {
     private String cargo;
     private String nivel_acesso;
     
+    //Getters e Setters
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getNivel_acesso() {
+        return nivel_acesso;
+    }
+
+    public void setNivel_acesso(String nivel_acesso) {
+        this.nivel_acesso = nivel_acesso;
+    }
     
 }
