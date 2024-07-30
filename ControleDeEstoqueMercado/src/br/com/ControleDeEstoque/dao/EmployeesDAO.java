a0d0a3223a1b13294ea9b8a89f577b6f7ca83637
Lívia Marques <livia.rodrigues.pb@compasso.com.br>
2024-07-30 19:02:11 -0300

    feat: implementando método cadastrarFuncionario

--- ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/EmployeesDAO.java
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/EmployeesDAO.java
@@ -5,7 +5,11 @@
 package br.com.ControleDeEstoque.dao;
 
 import br.com.ControleDeEstoque.jdbc.ConnectionFactory;
+import br.com.ControleDeEstoque.model.Employees;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -20,4 +24,34 @@ public class EmployeesDAO {
         this.con = new ConnectionFactory().getConnection();
     }
     
+    //Método cadastrar funcionario
+    public void cadastrarFuncionario(Employees obj) {
+        String sql = """
+                     INSERT INTO tb_funcionarios(nome, rg, cpf, email, senha, cargo, nivel_acesso telefone, celular, cep, endereco, numero, complemento, bairro, cidade, estado)
+                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""";
+        try (PreparedStatement stmt = con.prepareStatement(sql)) {
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getSenha());
+            stmt.setString(6, obj.getCargo());
+            stmt.setString(7, obj.getNivel_acesso());
+            stmt.setString(8, obj.getTelefone());
+            stmt.setString(9, obj.getCelular());
+            stmt.setString(10, obj.getCep());
+            stmt.setString(11, obj.getEndereco());
+            stmt.setInt(12, obj.getNumero());
+            stmt.setString(13, obj.getComplemento());
+            stmt.setString(14, obj.getBairro());
+            stmt.setString(15, obj.getCidade());
+            stmt.setString(16, obj.getUf());
+
+            stmt.execute();
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
+        }
+    }
 }
