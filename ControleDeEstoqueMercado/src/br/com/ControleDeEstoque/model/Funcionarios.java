7812e9531fa791cc6bb091b7eee751be32246bef
Lívia Marques <livia.rodrigues.pb@compasso.com.br>
2024-07-30 18:41:37 -0300

    feat: criando classe Funcionarios e adicionando relação de herança a classe Clientes

---
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/model/Funcionarios.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ControleDeEstoque.model;
+
+/**
+ *
+ * @author livia
+ */
+public class Funcionarios extends Clientes {
+    
+    //Atributos que não temos em cliente
+    
+}
