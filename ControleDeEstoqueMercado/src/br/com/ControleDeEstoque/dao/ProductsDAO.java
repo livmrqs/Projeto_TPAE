831f18828629399ab2de61eb415c3d5ca5d640fc
Lívia Marques <livia.rodrigues.pb@compasso.com.br>
2024-08-06 14:43:18 -0300

    feat: criando classe ProductsDAO e fazendo conexão com o db

---
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/ProductsDAO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ControleDeEstoque.dao;
+
+import br.com.ControleDeEstoque.jdbc.ConnectionFactory;
+import java.sql.Connection;
+
+/**
+ *
+ * @author livia
+ */
+public class ProductsDAO {
+
+    private Connection con;
+
+    public ProductsDAO() {
+        this.con = new ConnectionFactory().getConnection();
+    }
+}
