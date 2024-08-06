519d34cf053f2c08e71f913390b08704d09103a5
Lívia Marques <livia.rodrigues.pb@compasso.com.br>
2024-08-06 11:19:54 -0300

    feat: criando método listarFornecedor

--- ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/SuppliersDAO.java
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/SuppliersDAO.java
@@ -5,10 +5,14 @@
 package br.com.ControleDeEstoque.dao;
 
 import br.com.ControleDeEstoque.jdbc.ConnectionFactory;
+import br.com.ControleDeEstoque.model.Clientes;
 import br.com.ControleDeEstoque.model.Suppliers;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -49,7 +53,62 @@ public class SuppliersDAO {
             JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
         }
     }
+    // Método Alterar Fornecedor
+
+    public void alterarFornecedor(Suppliers obj) {
+        String sql = "UPDATE tb_fornecedores SET nome=?, cnpj=?, email=?, telefone=?, celular=?, cep=?, endereco=?, numero=?, complemento=?, bairro=?, cidade=?, estado=? WHERE id=?";
+        try (PreparedStatement stmt = con.prepareStatement(sql)) {
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getCnpj());
+            stmt.setString(3, obj.getEmail());
+            stmt.setString(4, obj.getTelefone());
+            stmt.setString(5, obj.getCelular());
+            stmt.setString(6, obj.getCep());
+            stmt.setString(7, obj.getEndereco());
+            stmt.setInt(8, obj.getNumero());
+            stmt.setString(9, obj.getComplemento());
+            stmt.setString(10, obj.getBairro());
+            stmt.setString(11, obj.getCidade());
+            stmt.setString(12, obj.getUf());
+            stmt.setInt(13, obj.getId());
+
+            stmt.execute();
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
+        }
+    }
     
+    // Método Listar Fornecedor
+    public List<Suppliers> listarFornecedor() {
+        List<Suppliers> lista = new ArrayList<>();
+        String sql = "SELECT * FROM tb_fornecedores";
+        try (PreparedStatement stmt = con.prepareStatement(sql); ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                Suppliers obj = new Suppliers();
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setCnpj(rs.getString("cnpj"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+
+                lista.add(obj);
+            }
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
+        }
+        return lista;
+    }
+
     // Método Excluir Fornecedor
     public void excluirFornecedor(Suppliers obj) {
         String sql = "DELETE FROM tb_fornecedores WHERE id=?";
