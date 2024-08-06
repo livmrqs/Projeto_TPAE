c5f2e239734734602889eb872c21424ac60a06aa
Lívia Marques <livia.rodrigues.pb@compasso.com.br>
2024-08-06 10:49:40 -0300

    feat: criando método cadastrarFornecedor

--- ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/SuppliersDAO.java
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/SuppliersDAO.java
@@ -5,7 +5,11 @@
 package br.com.ControleDeEstoque.dao;
 
 import br.com.ControleDeEstoque.jdbc.ConnectionFactory;
+import br.com.ControleDeEstoque.model.Suppliers;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,5 +21,32 @@ public class SuppliersDAO {
 
     public SuppliersDAO() {
         this.con = new ConnectionFactory().getConnection();
+    }
+
+    // Método Cadastrar Fornecedor
+    public void cadastrarFornecedor(Suppliers obj) {
+        String sql = """
+                     INSERT INTO tb_fornecedores(nome, cnpj, email, telefone, celular, cep, endereco, numero, complemento, bairro, cidade, estado)
+                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""";
+        try (PreparedStatement stmt = con.prepareStatement(sql)) {
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getCnpj());
+            stmt.setString(3, obj.getEmail());
+            stmt.setString(4, obj.getTelefone());
+            stmt.setString(5, obj.getCelular());
+            stmt.setString(6, obj.getCep());
+            stmt.setString(7, obj.getEndereco());
+            stmt.setInt(8, obj.getNumero());
+            stmt.setString(9, obj.getComplemento());
+            stmt.setString(10, obj.getBairro());
+            stmt.setString(11, obj.getCidade());
+            stmt.setString(12, obj.getUf());
+
+            stmt.execute();
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
+        }
     }
 }
