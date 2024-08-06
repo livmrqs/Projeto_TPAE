c64ec7e2826bea1532802eebc5d39c08c011c196
Lívia Marques <livia.rodrigues.pb@compasso.com.br>
2024-08-06 10:42:21 -0300

    feat: adicionando atributos, getters e setter de Suppliers

--- ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/model/Suppliers.java
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/model/Suppliers.java
@@ -10,4 +10,16 @@ package br.com.ControleDeEstoque.model;
  */
 public class Suppliers extends Clientes {
     
+    //atributos
+    private String cnpj;
+    
+    //getters e setters
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
 }
