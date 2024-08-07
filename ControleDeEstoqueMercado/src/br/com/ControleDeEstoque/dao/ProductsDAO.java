f80f0737f93b724b5c2c042d30cbe06a9fef6198
livmrqs <livia.rodrigues3@estudante.ufla.br>
2024-08-07 08:43:54 -0300

    feat: criando método listarProdutosPorNome

--- ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/ProductsDAO.java
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/ProductsDAO.java
@@ -92,7 +92,41 @@ public class ProductsDAO {
       
 }
     //Método listar produtos por nome
-    
+    public List<Products> listarProdutosPorNome(String nome) {
+        
+        try { 
+            
+            List<Products> lista = new ArrayList<>();
+        String sql = "select p.id, p.descricao, p.preco, p.qtd_estoque, f.nome from tb_produtos as p "
+                + "inner join tb_fornecedores as f on (p.for_id = f.id) where p.descricao like?";
+        
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, nome);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Products obj = new Products();
+                Suppliers f = new Suppliers();
+                
+                obj.setId(rs.getInt("p.id"));
+                obj.setDescricao(rs.getString("p.descricao"));
+                obj.setPreco(rs.getDouble("p.preco"));
+                obj.setQtd_estoque(rs.getInt("p.qtd_estoque"));
+                
+                f.setNome(rs.getString(("f.nome")));
+                
+                obj.setFornecedor(f);
+
+                lista.add(obj);
+            }
+                return lista;
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
+            return null;
+        }
+      
+    }
     //Método alterar produto
     public void alterarProdutos(Products obj) {
         
