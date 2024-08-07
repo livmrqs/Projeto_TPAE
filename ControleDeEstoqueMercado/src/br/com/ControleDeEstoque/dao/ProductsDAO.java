e8c59aef521abe20673892edae90275c74509f46
livmrqs <livia.rodrigues3@estudante.ufla.br>
2024-08-07 08:18:27 -0300

    feat: criando método listarProdutos

--- ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/ProductsDAO.java
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/ProductsDAO.java
@@ -6,9 +6,13 @@ package br.com.ControleDeEstoque.dao;
 
 import br.com.ControleDeEstoque.jdbc.ConnectionFactory;
 import br.com.ControleDeEstoque.model.Products;
+import br.com.ControleDeEstoque.model.Suppliers;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -39,4 +43,42 @@ public class ProductsDAO {
             JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
         }
 }
+    //Método excluir produto
+    
+    //Método listar produtos
+    public List<Products> listarProdutos() {
+        
+        try { 
+            
+            List<Products> lista = new ArrayList<>();
+        String sql = "select p.id, p.descricao, p.preco, p.qtd_estoque, f.nome from tb_produtos as p "
+                + "inner join tb_fornecedores as f on (p.for_id = f.id)";
+        
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Products obj = new Products();
+                Suppliers f = new Suppliers();
+                
+                obj.setId(rs.getInt("p.id"));
+                obj.setDescricao(rs.getString("p.descricao"));
+                obj.setPreco(rs.getDouble("p.preco"));
+                obj.setQtd_estoque(rs.getInt("p.qtd_estoque"));
+                
+                f.setNome(rs.getString(("f.nome")));
+                
+                obj.setFornecedor(f);
+
+                lista.add(obj);
+            }
+                return lista;
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
+            return null;
+        }
+      
+}
+    //Método listar produtos por nome
 }
