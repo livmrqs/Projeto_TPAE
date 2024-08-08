1ebd89618fa0dbda93b5cd10d19204d0eea8e207
livmrqs <livia.rodrigues3@estudante.ufla.br>
2024-08-08 14:37:58 -0300

    fix: corrigindo classe do método listarItensPorId

--- ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/SalesItensDAO.java
+++ ControleDeEstoqueMercado/src/br/com/ControleDeEstoque/dao/SalesItensDAO.java
@@ -5,10 +5,15 @@
 package br.com.ControleDeEstoque.dao;
 
 import br.com.ControleDeEstoque.jdbc.ConnectionFactory;
+import br.com.ControleDeEstoque.model.Products;
 import br.com.ControleDeEstoque.model.SalesItens;
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
 import javax.swing.JOptionPane;
+import java.sql.SQLException;
 
 /**
  *
@@ -39,4 +44,41 @@ public class SalesItensDAO {
         }
        
     }
+    ////metodo que lista itens de uma venda por id
+    public List<SalesItens> listarItensPorId(int venda_id) {
+        
+        try { 
+            
+            List<SalesItens> lista = new ArrayList<>();
+        String sql = "select i.id, p.descricao, i.qtd, p.preco, i.subtotal from tb_itensvendas as i "
+                 + "inner join tb_produtos as p on(i.produto_id = p.id) where i.venda_id=?";
+        
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, venda_id);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                SalesItens item = new SalesItens();
+                Products prod = new Products();
+                
+                item.setId(rs.getInt("i.id"));
+                prod.setDescricao(rs.getString("p.descricao"));
+                item.setSubtotal(rs.getInt("i.subtotal"));
+                prod.setPreco(rs.getDouble("p.preco"));
+                item.setQtd(rs.getInt("i.qtd"));
+                
+                item.setProdutos(prod);
+                
+                lista.add(item);
+                
+            }
+                return lista;
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro.getMessage());
+            return null;
+        }
+    
+}
 }
